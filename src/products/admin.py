8457809feef125a72d2682357d5086975a9c9416
Akshat <akshat54@gmail.com>
2016-12-19 11:49:39 +0530

    product detail layout created

---
+++ src/products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Product, Variation
+
+admin.site.register(Product)
+admin.site.register(Variation)
---
+++ src/products/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+
+from .views import ProductDetailView, ProductListView
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'newsletter.views.home', name='home'),
+    url(r'^(?P<pk>\d+)', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^$', ProductListView.as_view(), name='product_list'),
+    # url(r'^(?P<id>\d+)', 'products.views.product_detail_view_func', name='product_detail_function'),
+]
