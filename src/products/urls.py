547916d20d26876b8155aa0b863247b8a3c10a7d
Akshat <akshat54@gmail.com>
2016-12-19 13:35:47 +0530

    search added

--- src/products/urls.py
+++ src/products/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^(?P<pk>\d+)', ProductDetailView.as_view(), name='product_detail'),
-    url(r'^$', ProductListView.as_view(), name='product_list'),
+    url(r'^$', ProductListView.as_view(), name='products'),
     # url(r'^(?P<id>\d+)', 'products.views.product_detail_view_func', name='product_detail_function'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
+from django.db.models import Q
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.utils import timezone
 
 from .models import Product
 
@@ -11,3 +13,26 @@ class ProductDetailView(DetailView):
 
 class ProductListView(ListView):
     model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        context["now"] = timezone.now()
+        context["query"] = self.request.GET.get("q")
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProductListView, self).get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")
+        if query:
+            qs = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+            try:
+                qs2 = self.model.objects.filter(
+                    Q(price=query)
+                )
+                qs = (qs | qs2).distinct()
+            except:
+                pass
+        return qs
